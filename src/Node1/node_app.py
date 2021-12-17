e2fbe32f6c0ea433ee41fe1469c48d719647ad22
fseyedmousavi <faezehseyedmousavi@gmail.com>
2021-12-17 12:21:41 +0330

    find_closest function

--- src/Node1/node_app.py
+++ src/Node1/node_app.py
@@ -1,6 +1,6 @@
 from requests.api import request
 import yaml
-from flask import Flask
+from flask import Flask, jsonify
 import shutil
 import requests
 import logging
@@ -8,56 +8,62 @@ import logging
 
 app = Flask(__name__)
 
-
 file1 = open("Config.yml")
 current_node = yaml.load(file1, Loader=yaml.FullLoader)
 
 nodes_visited = []
-friend_counter = 0
 file_name = ""
 
+logging.getLogger().setLevel(logging.INFO)
 
-def request(next_node: dict, friend_counter: int):
+
+@app.route('/<file_requested>/<node_number>', methods=['GET'])
+def response(file_requested, nnu):
+    if current_node['owned_files'].__contains__(file_requested):
+        return jsonify({"string": '../Node' + str(current_node['node_number']) + '/ownedFiles/' + str(file_requested)})
+    else:
+        nodes: list = current_node['friend_nodes']
+        x = [i for i in nodes if not (i['node_name'] == int(nnu))]
+        return jsonify({"dict": find_closest(current_node['node_name'], nodes)})
+
+def find_closest(current_number: int, friends: list):
+    closest = friends[0]
+
+    for f in friends:
+        x = closest['node_name'] - current_number
+        y = f['node_name'] - current_number
+        if x > y:
+            closest = f
+    return closest
+
+def request(next_node: dict):
     # reply is a string of file path (type: str)
     # if another node was returned (type: dict)
-    reply = requests.get("http://127.0.0.1:" + str(
-        next_node['node_port']) + '/' + file_name + '/' + str(current_node['node_number']))
-    if (type(reply) == dict):
+    request_str = "http://127.0.0.1:" + str(next_node['node_port']) + '/' + str(file_name) + '/' + str(current_node['node_number'])
+    reply = requests.get(request_str)
+    rep = reply.json()
+    if rep.__contains__("dict"):
         nodes_visited.append(next_node)
-        if (nodes_visited.__contains__(reply)):
-            request(current_node['friend_nodes']
-                    [friend_counter], friend_counter+1)
+        if nodes_visited.__contains__(rep["dict"]):
+            request(find_closest(current_node['node_name'], current_node['friend_nodes']))
         else:
             logging.info("another path to node number " +
-                         reply['node_name'] + "was introduced")
-            request(reply, friend_counter)
-    elif (type(reply) == str):
-        original = r'' + reply
-        target = r'' + 'Node' + \
-            current_node['node_number'] + '/newFiles/' + file_name
+                         str(rep["dict"]['node_name']) + "was introduced")
+            request(rep["dict"])
+    elif rep.__contains__("string"):
+        original = r'' + rep["string"]
+        target = r'' + '../Node' + str(current_node['node_number']) + '/newFiles/' + file_name
         shutil.copyfile(original, target)
         logging.info("file was found and placed successfully")
         nodes_visited.clear()
 
 
-@app.route('/', methods=['GET'])
-def response(file_requested, node_number):
-    node_number_requested = int(node_number)
-    if (current_node['owned_files'].__contains__(file_requested)):
-        return 'Node' + current_node['node_number'] + '/ownedFiles/' + file_requested
-    else:
-        nodes: list = current_node['friend_nodes']
-        for node in nodes:
-            if not (node['node_name'] == node_number_requested):
-                return node
-
-
-string = input()
-if not(string.__contains__('request')):
-    print('INVALID INPUT')
+string = input("make a request:")
+if not (string.__contains__('request')):
+    logging.info('INVALID INPUT')
 else:
     file_name = string[8:]
-    request(current_node['friend_nodes'][friend_counter], friend_counter + 1)
+    request(find_closest(current_node['node_number'], current_node['friend_nodes']))
 
 if __name__ == "__main__":
     app.run(port=current_node['node_port'])
